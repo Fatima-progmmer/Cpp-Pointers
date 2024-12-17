7c01a9b2b9ef353be8ed64a78e9f8df34a8cb3ea
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2024-12-17 15:12:02 +0500

    Add files via upload

---
+++ 1.cxx
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int var1=11;
+int var2= 22;
+int var3=33;
+cout << "1st address is "<< &var1 <<endl;
+cout << "2nd address is " << &var2 <<endl;
+cout <<"3rd address is " << &var3 <<endl;
+return 0;	
+}
